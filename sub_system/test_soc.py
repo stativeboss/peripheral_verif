96c3b23f9602f4fc8d6a4a6f7da2989d5cb62597
Shiva <irivinti6@gmail.com>
2026-01-13 14:31:08 +0000

    run

--- sub_system/test_soc.py
+++ sub_system/test_soc.py
@@ -11,7 +11,7 @@ import logging
 ## SoC Memory Map: https://gitlab.com/shaktiproject/cores/c-class/-/blob/master/test_soc/c64_c32/Soc.defines?ref_type=heads
 ## UART Register details: https://gitlab.com/shaktiproject/uncore/devices/-/blob/master/uart_v2/uart.defines?ref_type=heads
 
-## CoCoTb Extensions: 
+## CoCoTb Extensions:
 ## https://github.com/alexforencich/cocotbext-axi/tree/master
 ## https://github.com/alexforencich/cocotbext-uart
 
@@ -19,7 +19,7 @@ import logging
 @cocotb.test()
 async def test_peripherals(dut):
     """Test to verify uart through AXI4 transactions"""
-    clock = Clock(dut.CLK, 100, unit="ns")  # Create a 10us period clock on port clk
+    clock = Clock(dut.CLK, 100)  # Create a 10us period clock on port clk
     # Start the clock. Start it low to avoid issues on the first RisingEdge
     cocotb.start_soon(clock.start(start_high=False))
     dut.RST_N.value = 0
